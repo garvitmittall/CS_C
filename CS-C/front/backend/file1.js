74c0df0beb29d80aa89b8ebbe37e4c068977b46d
garvitmittal.22@gmail.com <garvitmittal.22@gmail.com>
2025-02-07 12:21:06 +0530

    Updated on 07-02-25

--- CS-C/front/backend/file1.js
+++ CS-C/front/backend/file1.js
@@ -1,3 +1,15 @@
 const fs = require("fs");
 const data = fs.readFileSync("./data.txt","utf8");
 console.log(data);
+if(data.match("H")) {
+    console.log("H is present")
+    const newdata = data.replace("H","AB")
+    fs.writeFileSync("./data.txt",newdata, "utf-8");
+}
+
+// fs.writeFileSync("./data1.txt","Hello, this is my new work","utf-8");
+
+// fs.writeFileSync("./data1.txt","ABES","utf-8");
+
+// fs.appendFileSync("./data1.txt","Engineering College","utf-8");
+// fs.renameSync("./data2.txt","./data3.txt");
